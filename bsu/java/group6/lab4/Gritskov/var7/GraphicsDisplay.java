c7908e3597ca97b157d4dc5f7c341408edd61c50
Denis Greckij <denisgreckij38@gmail.com>
2021-12-14 18:58:27 +0300

    Основа приложения

---
+++ bsu/java/group6/lab4/Gritskov/var7/GraphicsDisplay.java
@@ -0,0 +1,50 @@
+package bsu.java.group6.lab4.Gritskov.var7;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Paint;
+import java.awt.Stroke;
+import java.awt.font.FontRenderContext;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.GeneralPath;
+import java.awt.geom.Line2D;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+import javax.swing.JPanel;
+
+public class GraphicsDisplay extends JPanel{
+	
+	private Double[][] graphicsData;
+	
+	private boolean showAxis = true;
+	private boolean showMarkers = false;
+	
+	private double minX;
+	private double maxX;
+	private double minY;
+	private double maxY;
+	
+	private double scale;
+	
+	private BasicStroke graphicsStroke;
+	private BasicStroke axisStroke;
+	private BasicStroke markerStroke;
+	
+	private Font axisFont;
+	
+	
+	
+	public GraphicsDisplay() {
+		setBackground(Color.WHITE);
+		float[] dash = {10.0f, 10.0f};
+		graphicsStroke = new BasicStroke(2.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_ROUND, 10.0f, dash, 0.0f);
+		axisStroke = new BasicStroke(2.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER, 10.0f, null, 0.0f);
+		markerStroke = new BasicStroke(2.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER, 10.0f, null, 0.0f);
+		axisFont = new Font("Serif", Font.BOLD, 36);
+		
+	}
+
+}
---
+++ bsu/java/group6/lab4/Gritskov/var7/MainFrame.java
@@ -0,0 +1,72 @@
+package bsu.java.group6.lab4.Gritskov.var7;
+
+import java.awt.BorderLayout;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JOptionPane;
+import javax.swing.event.MenuEvent;
+import javax.swing.event.MenuListener;
+
+public class MainFrame extends JFrame {
+
+	private static final int WIDTH = 800;
+	private static final int HEIGHT = 600;
+	private JFileChooser fileChooser = null;
+	private JCheckBoxMenuItem showAxisMenuItem;
+	private JCheckBoxMenuItem showMarkersMenuItem;
+	private GraphicsDisplay display = new GraphicsDisplay();
+	private boolean fileLoaded = false;
+	
+	public MainFrame() {
+		super("Построение графиков функций на основе подготовленных файлов");
+		setSize(WIDTH, HEIGHT);
+		Toolkit kit = Toolkit.getDefaultToolkit();	
+		setLocation((kit.getScreenSize().width - WIDTH)/2, (kit.getScreenSize().height - HEIGHT)/2);
+		setExtendedState(MAXIMIZED_BOTH);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		JMenu fileMenu = new JMenu("File");
+		menuBar.add(fileMenu);
+		Action openGraphicsAction = new AbstractAction("Open") {
+			public void actionPerformed(ActionEvent event) {
+			if (fileChooser==null) {
+				fileChooser = new JFileChooser();
+				fileChooser.setCurrentDirectory(new File("."));
+			}
+			if (fileChooser.showOpenDialog(MainFrame.this) == JFileChooser.APPROVE_OPTION)
+				openGraphics(fileChooser.getSelectedFile());
+			}
+		};
+		fileMenu.add(openGraphicsAction);
+		JMenu graphicsMenu = new JMenu("Graphic");
+		menuBar.add(graphicsMenu);
+		
+		
+		
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		MainFrame frame = new MainFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+	
+	
+}
